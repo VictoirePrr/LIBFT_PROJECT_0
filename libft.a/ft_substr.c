2342aac5f6fb63da3401cb69f8d124e719ec6b39
Victoire Perrier <vicperri@z2r10p1.42lyon.fr>
2024-11-26 15:34:53 +0100

    MODIF : substr return null instead of 0

--- libft.a/ft_substr.c
+++ libft.a/ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: vicperri <vicperri@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 13:28:07 by vicperri          #+#    #+#             */
-/*   Updated: 2024/11/15 13:16:54 by vicperri         ###   ########lyon.fr   */
+/*   Updated: 2024/11/26 15:33:39 by vicperri         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		len = ft_strlen(s) - start;
 	str = (char *)malloc(len + 1 * sizeof(char));
 	if (!(str))
-		return (0);
+		return (NULL);
 	while (s[start] && len--)
 		str[i++] = s[start++];
 	str[i] = '\0';
